1d96bde599ab13958e7928ab23e11a3cd9e404e5
Minerva1337 <lukas.holzmann2002@gmail.com>
2025-06-10 17:49:34 +0200

    Debugging

--- logic_layer/test_createExcersie.ts
+++ logic_layer/test_createExcersie.ts
@@ -1,13 +1,17 @@
 import { createEntry } from './db-utils';
 
 async function run() {
-  const success = await createEntry('exercises', {
+    const success = await createEntry('exercises', {
     user_id: 5,
     name: 'Trizeps Kickbacks',
     category: 'Trizeps'
-  });
+    });
+
 
   console.log(success ? '✅ Eintrag erstellt' : '❌ Fehler beim Erstellen');
 }
 
 run();
+
+
+
