461dc4b40075f543e44af2df030cefbf43e60f66
Minerva <lukas.holzmann2002@gmail.com>
2025-06-11 19:53:00 +0200

    Abhängiigkeiten korrekt definiert

--- backend/common/decorators/user.decorator.ts
+++ backend/common/decorators/user.decorator.ts
@@ -1,6 +1,6 @@
-// ========== user.decorator.ts ==========
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+// Ein Dekorator, der den Benutzer aus dem Request extrahiert
 export const User = createParamDecorator((_, ctx: ExecutionContext) => {
   const req = ctx.switchToHttp().getRequest();
   return req.user;
